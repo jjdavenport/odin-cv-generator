560c26efe7db3de5045361cb50e35a3fdbb20da3
jjdavenport <jordanjamesdavenport@gmail.com>
2025-01-23 17:06:59 +0000

    fix: overflow-y on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
             certificates={certificates}
           />
         </div>
-        <div className="w-full overflow-hidden overflow-x-hidden md:w-1/2 md:overflow-y-auto">
+        <div className="w-full overflow-x-hidden md:w-1/2 md:overflow-y-auto">
           <Inputs
             deleteExperience={deleteExperience}
             deleteSkill={deleteSkill}
